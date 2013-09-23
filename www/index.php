8867f983227d96ba81357fdd5f1710ec8623e5de
Rigo Rojas <rigo.rojas@gmail.com>
2013-09-23 11:51:40 -0700

    Submitting code.

--- www/index.php
+++ www/index.php
@@ -1,1 +1,16 @@
-I love it
+Right click in a blank area of the project explorer
+
+Select import from dropdown
+
+Git -> Git repository as new project
+
+Select -> URI ->
+
+Do some work
+
+
+Do some work in Aptana,
+add files, folder, etc.
+then in Aptana's project browser
+right click on the project name,
+team - stage, commit, push (in that order)
