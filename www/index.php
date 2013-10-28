83b0f62caa5745daa4d503c88f9467585c4a90db
Rigo Rojas <rigo.rojas@nfl.com>
2013-10-27 23:02:21 -0700

    Complete HTML Structure

--- www/index.php
+++ www/index.php
@@ -1,1 +1,14 @@
-My New Site
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Amazing CMS</title>
+</head>
+<body>
+	<div id="global">
+		<div id="left" class="column">left</div>
+		<div id="content" class="column">content</div>
+		<div id="right" class="column">right</div>
+	</div><!--end  #global-->
+</body>
+</html>
